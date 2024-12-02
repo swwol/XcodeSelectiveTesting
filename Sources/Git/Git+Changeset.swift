ca9c4f6a95f04c60aa7ee4ac071420d7114fd086
sam woolf <sam.woolf@gmail.com>
2024-12-02 15:53:09 +0000

    logging

--- Sources/Git/Git+Changeset.swift
+++ Sources/Git/Git+Changeset.swift
@@ -25,12 +25,16 @@ public extension Git {
 
         let changes = try Shell.execOrFail("cd \(path) && git diff '\(baseBranch)'..'\(currentBranch)' --name-only")
         let changesTrimmed = changes.trimmingCharacters(in: .whitespacesAndNewlines)
-
+        Logger.message("changes trimmed: " + changesTrimmed)
         guard !changesTrimmed.isEmpty else {
             return Set()
         }
 
-        return Set(changesTrimmed.components(separatedBy: .newlines).map { gitRoot + $0 })
+     let set = Set(changesTrimmed.components(separatedBy: .newlines).map { gitRoot + $0 })
+      let set2 = Set(changesTrimmed.split(whereSeparator: \.isNewline).map { gitRoot + String($0) })
+      Logger.message("set contains \(set.count)")
+      Logger.message("set2 contains \(set2.count)")
+      return set
     }
 
     func localChangeset() throws -> Set<Path> {
